e70304b70782f3f6497110c0272fad20ae82dd20
Özgecan Muyanlı <ozgecan.muyanli@tai.com.tr>
2021-11-02 08:53:18 +0300

    Add vertex normals as attribute and add light
    A light is added to 0,0,0 position. A cube object has 6 vertex normals.
    Therefore, vertex normals are added into vertices array as vertex
    attribute. They are multiplied with modelMatrix in vertex shader.
    However, w is set to 0, not 1. Because vertex normals should not be
    affected by translate operation.

--- Shaders/myShader.fs
+++ Shaders/myShader.fs
@@ -4,6 +4,7 @@ in vec2 oTextureCoord;
 
 in vec3 oPos; // object's position in local space
 in vec3 oPosInWorldSpace; // object's position in world space
+in vec3 oVertexNormal;
 
 uniform vec3 objectColor;
 uniform float lightAngle;
@@ -11,15 +12,16 @@ uniform sampler2D texture1;
 
 void main()
 {
-	vec3 lightPos;// = vec3(0.0f, 0.0f, 0.25f);
-	lightPos.x = cos(lightAngle) / 2.0f;
-	lightPos.y = sin(lightAngle) / 2.0f;
-	lightPos.z = 0.25f;
+	vec3 lightPos = vec3(0.0f, 0.0f, 0.0f);
+	// lightPos.x = cos(lightAngle) / 2.0f;
+	// lightPos.y = sin(lightAngle) / 2.0f;
+	// lightPos.z = 0.0;
 
-	vec3 vertexNormal = vec3(0.0f, 0.0f, 1.0f); // object's normal vec
+	vec3 vertexNormal = normalize(oVertexNormal);
 	vec3 lightDirection = normalize(lightPos - oPosInWorldSpace);
 	float lightIntense = dot(lightDirection, vertexNormal);
 	vec3 finalColor = objectColor * lightIntense;
 
-	FragColor = texture(texture1, oTextureCoord); // output color and alpha
+	//FragColor = vec4(vertexNormal, 1.0f);
+	FragColor = lightIntense * texture(texture1, oTextureCoord); // output color and alpha
 }
